f27c19ee5413b9d9434a566a52da23218ebe76ef
Michael S <hakushigo@hotmail.com>
2024-05-29 15:12:00 +0700

    push

--- middleware/token.go
+++ middleware/token.go
@@ -11,19 +11,25 @@ We will do token checking over here.
 */
 
 func TokenLogin(ctx *fiber.Ctx) error {
-	authorizationKey := ctx.Accepts("Authorization") // get the used auth key
+	authorizationKey := ctx.Get("Authorization") // get the used auth key
+	if authorizationKey == "" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": fiber.StatusUnauthorized,
+			"error":  "Authorization is needed",
+		})
+	}
 
 	// try to find the key data
 	var tokendata model.Token
-	fetched := db.Where(&model.Token{Token: authorizationKey}).First(&tokendata).RowsAffected
+	find := db.Where(&model.Token{Token: authorizationKey}).Find(&tokendata).RowsAffected
 
 	// if none
-	if fetched == 0 && tokendata.ExpiredAt.Before(time.Now()) && tokendata.IsEnabled {
+	if find == 0 || tokendata.ExpiredAt.Before(time.Now()) || !tokendata.IsEnabled {
 		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"status": fiber.StatusUnauthorized,
 			"error":  "Your token isn't valid or already expired",
 		})
+	} else {
+		return ctx.Next()
 	}
-
-	return ctx.Next()
 }
